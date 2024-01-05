c0aa8bdac26bf801c2a138dadb55cf11d1f04fc1
Marko Milic <milic@tyntec.com>
2024-01-05 15:13:54 +0100

    Update package.json and fix import in extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import * as cp from 'child_process';
 import * as vscode from 'vscode';
-import GoogleJavaFormattingProvider from './GoogleJavaFormattingProvider';
+import GoogleJavaFormattingProvider from './google-java-jormatting-provider';
 
 const documentFilter: vscode.DocumentFilter = {
 	language: "java",
@@ -9,15 +10,21 @@ const documentFilter: vscode.DocumentFilter = {
 };
 
 export function activate(context: vscode.ExtensionContext) {
+	cp.exec('java -version', (error, stdout, stderr) => {
+		if (error) {
+			vscode.window.showErrorMessage('Java is not available. Please install Java and try again.');
+			return;
+		}
 
-	const googleJavaFormatFilePath = context.asAbsolutePath('lib/google-java-format-1.19.1-all-deps.jar');
+		const googleJavaFormatFilePath = context.asAbsolutePath('lib/google-java-format-1.19.1-all-deps.jar');
 
-	context.subscriptions.push(
-		vscode.languages.registerDocumentRangeFormattingEditProvider(
-			documentFilter,
-			new GoogleJavaFormattingProvider(googleJavaFormatFilePath)
-		)
-	);
+		context.subscriptions.push(
+			vscode.languages.registerDocumentRangeFormattingEditProvider(
+				documentFilter,
+				new GoogleJavaFormattingProvider(googleJavaFormatFilePath)
+			)
+		);
+	});
 }
 
 // This method is called when your extension is deactivated
