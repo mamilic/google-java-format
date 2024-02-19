3605e6ee213cecdf0e8073f6f9e193ad808788f9
Marko Milic <milic@tyntec.com>
2024-02-19 18:40:49 +0100

    Update google-java-format to version 1.20.0

--- src/extension.ts
+++ src/extension.ts
@@ -16,7 +16,7 @@ export function activate(context: vscode.ExtensionContext) {
       return;
     }
 
-    const googleJavaFormatFilePath = context.asAbsolutePath('lib/google-java-format-1.19.2-all-deps.jar');
+    const googleJavaFormatFilePath = context.asAbsolutePath('lib/google-java-format-1.20.0-all-deps.jar');
 
     context.subscriptions.push(
       vscode.languages.registerDocumentRangeFormattingEditProvider(
